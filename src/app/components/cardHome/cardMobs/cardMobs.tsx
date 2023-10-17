6e065caed7321b64b85a9982981ca794534c9e4d
Rafael CDS <rafael.candidodasilva.pro@gmail.com>
2023-10-18 01:05:42 +0200

    ajout du squelette du composant cardMobs

--- src/app/components/cardHome/cardMobs/cardMobs.tsx
+++ src/app/components/cardHome/cardMobs/cardMobs.tsx
@@ -1,11 +1,32 @@
 import "../../styles/globals.css"
-import datas from "../../../datas/datas.json"
 
-export default function CardMobs() {
-    return (
-        <header>
-            <Logo/>
-            <Search />
-        </header> 
-    )
+interface CardMobsProps {
+  levelMin: number;
+  levelMax: number;
+  image: string;
+  name: string;
+  typeName: string;
 }
+
+const CardMobs: React.FC<CardMobsProps> = ({ levelMin, levelMax, image, name, typeName }) => {
+  return (
+    <div id="cardContainer">
+      <div id="rarity" />
+      <div id="level">
+        <div id="level">
+          Niv {levelMin} - {levelMax}
+          <div id="cardImage">
+            {image}
+            <div id="name">
+              {name}
+              <div id="typeImage" />
+              <div id="typeName">{typeName}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> 
+  )
+}
+
+export default CardMobs;
--- src/app/components/cardHome/home.tsx
+++ src/app/components/cardHome/home.tsx
@@ -1,4 +1,5 @@
 import "../../styles/globals.css"
+import datas from "../../datas/datas.json"
 
 export default function Home() {
     return (

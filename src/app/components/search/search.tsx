b2517f99dd2f0b1b5aff839bcbc73eefb56cd332
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-10-18 22:09:32 +0200

    Ajout de page Mobs et props placeholder Search

--- src/app/components/search/search.tsx
+++ src/app/components/search/search.tsx
@@ -3,10 +3,14 @@ import "./search.css"
 import searchIcon from "../../../../public/search-solid.svg"
 import Image from "../../../../node_modules/next/image"
 
-export default function Search() {
+interface Props {
+    placeholder: string
+}
+
+export default function Search({placeholder}: Props) {
     return (
         <div id="searchbarContainer">
-            <input type="text" placeholder="Rechercher un monstre, un équipement ou une ressource" id="searchBar" name="searchBar"/>
+            <input type="text" placeholder={placeholder} id="searchBar" name="searchBar"/>
             <label htmlFor="searchBar"><Image src={searchIcon} alt="search icon" id="searchIcon"/></label>
         </div> 
     )
---
+++ src/app/mobs/page.tsx
@@ -0,0 +1,11 @@
+import Logo from "../components/logo/logo";
+import Search from "../components/search/search";
+
+export default function Page() {
+  return (
+    <div>
+      <Logo />
+      <Search placeholder="Rechercher un monstre" />
+    </div>
+  )
+}

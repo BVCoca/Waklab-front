7ad7b0ef320ae858104bc8b50649db64e0522499
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-10-18 22:59:55 +0200

    Modification SearchComponent détection de l'URL

--- src/app/components/search/search.tsx
+++ src/app/components/search/search.tsx
@@ -1,16 +1,28 @@
+'use client'
+
 import "../../styles/globals.css"
 import "./search.css"
 import searchIcon from "../../../../public/search-solid.svg"
 import Image from "../../../../node_modules/next/image"
+import { usePathname } from 'next/navigation'
 
-interface Props {
-    placeholder: string
-}
+export default function Search() {
 
-export default function Search({placeholder}: Props) {
+    let pathname = usePathname();
+
+    if (pathname.includes("mobs")) {
+        pathname = "un monstre";
+    } else if(pathname.includes("resources")) {
+        pathname = "une ressource";
+    } else if(pathname.includes("stuffs")) {
+        pathname = "un équipement";
+    } else {
+        pathname = "un monstre, une ressource ou un équipement";
+    }
+
     return (
         <div id="searchbarContainer">
-            <input type="text" placeholder={placeholder} id="searchBar" name="searchBar"/>
+            <input type="text" placeholder={`Rechercher ${pathname}`} id="searchBar" name="searchBar"/>
             <label htmlFor="searchBar"><Image src={searchIcon} alt="search icon" id="searchIcon"/></label>
         </div> 
     )

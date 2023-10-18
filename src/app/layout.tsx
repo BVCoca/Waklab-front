81124500fc490b323866cde948c2cf819f85b6e6
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-10-18 22:14:27 +0200

    Ajout de la route Mobs dans le Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import "./styles/globals.css"
 import "./styles/menu.css"
 
@@ -7,29 +8,25 @@ export default function Menu({
     children: React.ReactNode
   }) {
   return (
-    <html lang="en">
-      <body suppressHydrationWarning={true}>
-        <div id="pageContent">
-          <div id="menuContainer">
-              <a id="logo" className="logosvg" href="#">
-                <img src={"/logo_menu.png"} alt="Logo Menu" style={{width: "30px"}}/>
-              </a>
-              <a id="logoRessources" className="logosvg" href="#">
-                <img src="/logoResources.svg" alt="Logo Ressources" />
-              </a>
-              <a id="logoStuffs" className="logosvg" href="#">
-                <img src="/logoStuffs.svg" alt="Logo Stuffs" />
-              </a>
-              <a id="logoMobs" className="logosvg" href="#">
-                <img src="/logoMobs.svg" alt="Logo Mobs" />
-              </a>
-              <a id="logoCac" className="logosvg" href="#">
-                <img src="/logoCaC.svg" alt="Logo CaC" />
-              </a>
-            </div>
-            {children}
-          </div>
-      </body>
-    </html>
+    <main>
+      <div id="menuContainer">
+          <a id="logo" className="logosvg" href="#">
+            <img src={"/logo_menu.png"} alt="Logo Menu" style={{width: "30px"}}/>
+          </a>
+          <a id="ressources" className="logosvg" href="#">
+            <img src="/logoResources.svg" alt="Logo Ressources" />
+          </a>
+          <a id="stuffs" className="logosvg" href="#">
+            <img src="/logoStuffs.svg" alt="Logo Stuffs" />
+          </a>
+          <Link href="/mobs" id="mobs" className="logosvg">
+            <img src="/logoMobs.svg" alt="Logo Mobs" />
+          </Link>
+          <a id="cac" className="logosvg" href="#">
+            <img src="/logoCaC.svg" alt="Logo CaC" />
+          </a>
+        </div>
+      {children}
+    </main>
   )
 }

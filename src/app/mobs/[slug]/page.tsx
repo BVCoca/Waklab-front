9913bbac58d86e36097cf24a0a81848edc4d01cd
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-10-19 13:27:02 +0200

    Récupération des infos de l'API dans mobDetails

--- src/app/mobs/[slug]/page.tsx
+++ src/app/mobs/[slug]/page.tsx
@@ -1,13 +1,60 @@
 import Image from "next/image";
+import datas from "@/app/datas/datas.json";
+import { getMob } from "@/app/services/mob";
+import { GetServerSidePropsContext } from "next";
+import MobSingle from "@/app/types/Mob/MobSingle";
 
+/* interface Props {
+    params : {
+        slug : string
+    }
+} */
 interface Props {
+    mobSingle: MobSingle; 
     params : {
         slug : string
     }
 }
 
-export default function MobDetails({params} : Props) {
+export default async function MobDetails({ params }: Props) {
 
-    return <p>Post: {params.slug}</p>
+    const mob = await getMob(params.slug);
+
+    if (!mob) {
+        return <p>Monstre non trouvé</p>;
+    }
+
+    return (
+        <div className="mobStatsContainer">
+        <div className="imageMobContainer">
+            <Image width={150} height={150} src={mob.imageUrl} alt="Image du monstre"/>
+            <h3>{mob.name}</h3>
+            <p>Famille: {mob.family.name}</p>
+        </div>
+
+        <div className="primaryStatsMobContainer">
+            <div className="HealthMobContainer">
+
+            </div>
+
+            <div className="helpStatsMobContainer">
+
+            </div>
+        </div>
+
+        <div className="secondaryStatsMobContainer">
+
+        </div>
+
+        <div className="infoMobContainer">
+            <div className="levelMobContainer">
+
+            </div>
+            <div className="isCapturable MobContainer">
+
+            </div>
+        </div>
+    </div>
+        )
     
 }

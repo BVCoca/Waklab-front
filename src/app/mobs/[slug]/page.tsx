65035f7fcdfa7108f5784036a2ebe81941a4c24b
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-10-19 23:30:33 +0200

    Modification composant mobDetails

--- src/app/mobs/[slug]/page.tsx
+++ src/app/mobs/[slug]/page.tsx
@@ -15,6 +15,21 @@ interface Props {
 export default async function MobDetails({ params }: Props) {
 
     const mob = await getMob(params.slug);
+    console.log(mob);
+
+    function nFormatter(num: number, digits:number): string {
+        const lookup = [
+          { value: 1, symbol: "" },
+          { value: 1e3, symbol: "k" },
+          { value: 1e6, symbol: "M" },
+        ];
+        const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+        var item = lookup.slice().reverse().find(function(item) {
+          return num >= item.value;
+        });
+        return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
+      }
+      
 
     if (!mob) {
         return <p>Monstre non trouvé</p>;
@@ -25,16 +40,19 @@ export default async function MobDetails({ params }: Props) {
         <HeaderDetails />
         <div className="mobStatsContainer">
             <div className="imageMobContainer">
-                <Image width={150} height={150} src={mob.imageUrl} alt={`Image du monstre ${mob.name}`}/>
+                <Image width={150} height={150} src={mob.imageUrl ? mob.imageUrl : "/mobDetailsIcon/healthHearth.svg"} alt={`Image du monstre ${mob.name}`}/>
                 <h3>{mob.name}</h3>
-                <p className="family">Famille : {mob.family.name}</p>
+                <p className="family">Famille : {mob.family ? mob.family.name : "Inconnu"}</p>
             </div>
 
             <div className="primaryStatsMobContainer">
                 <div className="healthMobContainer">
                     <div className="valueContainer">
-                        <Image src="/mobDetailsIcon/healthHearth.svg" alt="Image de la santé du monstre" width={100} height={93}/>
-                        <span id="valueHealth">{mob.hp}</span>
+                        <div className="tooltip">
+                            <span className="tooltiptext">{mob.hp.toLocaleString()} HP</span>
+                            <Image src="/mobDetailsIcon/healthHearth.svg" alt="Image de la santé du monstre" width={100} height={93}/>
+                            <span id="valueHealth">{nFormatter(mob.hp, 0)}</span>
+                        </div>
                     </div>
                     <div className="actionMovementContainer">
                         <div className="valueContainer">
@@ -46,11 +64,16 @@ export default async function MobDetails({ params }: Props) {
                             <span id="valueMovement">{mob.movementPoints}</span>
                         </div>
                     </div>
-
                 </div>
 
                 <div className="helpStatsMobContainer">
-
+                    <ul>
+                        <li className="lineValue"><Image src="/mobDetailsIcon/iconIni.png" alt="Icône Initiative" width={27} height={20}/><span>Initiative : </span><span className="valueHelpStats">{mob.initiative}</span></li>
+                        <li className="lineValue"><Image src="/mobDetailsIcon/iconTac.png" alt="Icône Tacle" width={27} height={20}/><span>Tacle : </span><span className="valueHelpStats">{mob.tackle}</span></li>
+                        <li className="lineValue"><Image src="/mobDetailsIcon/iconEsq.png" alt="Icône Esquive" width={27} height={20}/><span>Esquive : </span><span className="valueHelpStats">{mob.dodge}</span></li>
+                        <li className="lineValue"><Image src="/mobDetailsIcon/iconPar.png" alt="Icône Parade" width={27} height={20}/><span>Parade : </span><span className="valueHelpStats">{mob.parry} %</span></li>
+                        <li className="lineValue"><Image src="/mobDetailsIcon/iconCc.png" alt="Icône Coup Critique" width={27} height={20}/><span>Coup Critique : </span><span className="valueHelpStats">{mob.criticalHit} %</span></li>
+                    </ul>
                 </div>
             </div>
 

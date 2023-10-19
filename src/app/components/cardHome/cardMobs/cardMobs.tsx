f632113ae151b571f26a7a1dbf15289168d78b94
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-10-19 02:58:50 +0200

    Ajout du slug

--- src/app/components/cardHome/cardMobs/cardMobs.tsx
+++ src/app/components/cardHome/cardMobs/cardMobs.tsx
@@ -1,6 +1,7 @@
 import "@/app/styles/globals.css"
 import "./cardMobs.css"
 import Image from "next/image";
+import Link from "next/link";
 
 interface CardMobsProps {
   levelMin: number;
@@ -9,12 +10,14 @@ interface CardMobsProps {
   name: string;
   typeName: string;
   typeImage: string;
+  slug: string;
 }
 
-const CardMobs: React.FC<CardMobsProps> = ({ levelMin, levelMax, image, name, typeName, typeImage }) => {
+const CardMobs: React.FC<CardMobsProps> = ({ levelMin, levelMax, image, name, typeName, typeImage, slug }) => {
 
   return (
     <div id="cardContainer">
+      <Link href={`/mobs/${slug}`}>
       <div id="rarity" />
       <div id="level">
           Niv. {levelMin} - {levelMax}
@@ -29,6 +32,7 @@ const CardMobs: React.FC<CardMobsProps> = ({ levelMin, levelMax, image, name, ty
             <div id="typeName">{typeName}</div>
         </div>
       </div>
+      </Link>
     </div> 
   )
 }
--- src/app/mobs/page.tsx
+++ src/app/mobs/page.tsx
@@ -1,12 +1,27 @@
+import CardMobs from "../components/cardHome/cardMobs/cardMobs";
 import Header from "../components/header/header";
-import MobDetails from "../components/mobDetails/mobDetails";
-
+import datas from "@/app/datas/datas.json"
+import "../components/home/home.css"
 
 export default function Page() {
+  
   return (
     <div id="content">
       <Header />
-      <MobDetails />
+      <div id="cardList">
+      {datas && datas.map((data, index) => (
+      <CardMobs     
+        key={index}
+        levelMin={data.levelMin}
+        levelMax={data.levelMax}
+        image={data.imageUrl}
+        name={data.name}
+        typeName={data.famille}
+        typeImage={data.typeImage}
+        slug={data.slug}
+        />
+        ))}
+        </div>
     </div>
   )
 }

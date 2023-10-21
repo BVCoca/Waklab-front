79828bc96c2c1685d6cb0d809d0faad06c0a310a
Rafael CDS <rafael.candidodasilva.pro@gmail.com>
2023-10-21 13:27:18 +0200

    adding real sidebar component

--- src/app/components/common/ImageResizer.tsx
+++ src/app/components/common/ImageResizer.tsx
@@ -7,13 +7,13 @@ interface Props {
     alt : string
 }
 
-export default function ImageResizer(props :any) {
+export default function ImageResizer(props : any) {
 
-    let url = props.src.replace('/.w\d+h\d+.png/i', `w${props.width}h${props.height}`);
+    let url = props.src.replace('/.w\d+h\d+.png/i', `w${300}h${300}`);
 
     return (
         // Je désactive, car j'envoie tous les props
         // eslint-disable-next-line jsx-a11y/alt-text
-        <Image quality={100} {...props}/>
+        <Image quality={100} src={url} {...props}/>
     )
 }

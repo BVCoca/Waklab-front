1a6404209bf5ba89b2f722422936d7a9cce99304
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-10-21 17:11:41 +0200

    fix: weight type family css

--- src/app/components/common/Level.tsx
+++ src/app/components/common/Level.tsx
@@ -1,12 +1,17 @@
-import "./common.css"
+import "./common.css";
 
 interface Props {
-    level : number[],
-    isInCard? : boolean // Permet d'affiche Niv. ou Niveau
+  level: number[];
+  isInCard?: boolean; // Permet d'affiche Niv. ou Niveau
 }
 
-export default function Level({level, isInCard = true} : Props) {
-    return (
-        <h4 className="level">{isInCard ? 'Niv.' : 'Niveau'} {level.length > 1 ? `${level[0]} - ${level[1]}` : level[0]}</h4>
-    )
+export default function Level({ level, isInCard = true }: Props) {
+  return (
+    <h4 className="level">
+      {isInCard ? "Niv." : "Niveau"}{" "}
+      {level.length > 1 && level[0] != level[1]
+        ? `${level[0]} - ${level[1]}`
+        : level[0]}
+    </h4>
+  );
 }

19caa4cbd1d2b0ac40677cc9accbb5b2f8a35aab
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-10-21 15:20:01 +0200

    fix: Add params to arrow

--- src/app/components/headerDetails/headerDetails.tsx
+++ src/app/components/headerDetails/headerDetails.tsx
@@ -1,21 +1,26 @@
 import Logo from "../logo/logo";
-import arrow from "../../../../public/arrow.svg"
-import Image from "next/image"
+import arrow from "../../../../public/arrow.svg";
+import Image from "next/image";
 import Link from "next/link";
-import "./headerDetails.css"
+import "./headerDetails.css";
 
 export default function HeaderDetails() {
-    return (
-        <header>
-            <div className="containerRedirectBack">
-                <Link href="/mobs">
-                <div className="redirectBack">
-                    <Image src={arrow} alt="Flèche Retour en arrière" width={50} height={40}/>
-                    <p>Retour à la recherche</p>
-                </div>
-                </Link>
-            </div>
-            <Logo width={200} height={70}/>
-        </header> 
-    )
+  return (
+    <header>
+      <div className="containerRedirectBack">
+        <Link href="/mobs">
+          <div className="redirectBack">
+            <Image
+              src={arrow}
+              alt="Flèche Retour en arrière"
+              width={50}
+              height={40}
+            />
+            <p>Retour à la recherche</p>
+          </div>
+        </Link>
+      </div>
+      <Logo width={200} height={60} />
+    </header>
+  );
 }

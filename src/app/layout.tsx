bebd587a198abc11486f6a6892b8be14e390cf4d
Alexis Migniau <migniau.alexis@gmail.com>
2023-10-21 22:35:37 +0200

    fix(error) : Error lors du build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,29 @@
-import "./styles/globals.css"
-import "./styles/menu.css"
-import Menu from "./components/menu/menu"
+import "./styles/globals.css";
+import "./styles/menu.css";
+import Menu from "./components/menu/menu";
 
-export default function Layout({
-    children
-  }: {
-    children: React.ReactNode
-  }) {
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Waklab",
+  description: "Outils communautaires autour du jeu wakfu",
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main>
-      <div id="pageContent">
-        <div id="menuContainer">
-          <Menu/>
-        </div>
-        {children}
-      </div>
-    </main>
-  )
+    <html lang="fr">
+      <head>
+      </head>
+      <body>
+        <main>
+          <div id="pageContent">
+            <div id="menuContainer">
+              <Menu />
+            </div>
+            {children}
+          </div>
+        </main>
+      </body>
+    </html>
+  );
 }

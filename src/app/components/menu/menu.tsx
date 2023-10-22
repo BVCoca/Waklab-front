a567dd76cd29d751aa7fa347e3ec00455abcaba4
Rafael CDS <rafael.candidodasilva.pro@gmail.com>
2023-10-22 15:54:52 +0200

    style / changement de couleur et hover des icons sidebar

--- src/app/components/menu/menu.tsx
+++ src/app/components/menu/menu.tsx
@@ -1,33 +1,32 @@
 import "@/app/styles/globals.css"
 import "./menu.css"
-import MenuIcon from "./menuIcon/menuIcon"
-import MenuDesc from "./menuDesc/menuDesc"
+import LogoMobs from "@/app/icons/homepageIcon/logoMobs.svg"
+import LogoStuffs from "@/app/icons/homepageIcon/logoStuffs.svg"
+import LogoResources from "@/app/icons/homepageIcon/logoResources.svg"
+import Link from "next/link"
+import Image from "next/image"
 
 export default function Menu() {
-    
-    let srcLink = "/homepageIcon/logo"
 
     const params = [
-        {src:`${srcLink}_menu.png`, alt:``, href:`/`, label: ''},
-        {src:`${srcLink}Resources.svg`, alt:``, href:`/resources`, label: 'Ressources'},
-        {src:`${srcLink}Mobs.svg`, alt:``, href:`/mobs`, label: 'Monstres'},
-        {src:`${srcLink}Stuffs.svg`, alt:``, href:`/stuffs`, label: 'Équipements'}     
+        {src:<LogoResources/> , alt:``, href:`/resources`, label: 'Ressources'},
+        {src: <LogoMobs/>, alt:``, href:`/mobs`, label: 'Monstres'},
+        {src:<LogoStuffs/>, alt:``, href:`/stuffs`, label: 'Équipements'}     
     ]
 
     return (
-        params && params.map((param, index) => {
-            return (
-                <div className="menuWrapper" key={index}>
-                    <MenuIcon 
-                        src={param.src} 
-                        alt={param.alt} 
-                        href={param.href}
-                    />
-                    <MenuDesc 
-                        descIcon={param.label}
-                    />
-                </div>
-            )
-        })
+        <div id="menuContainer">
+            <Link href="/">
+                <Image  src="/homepageIcon/logo_menu.png" alt="" width={50} height={60}/>
+            </Link>
+            {params && params.map((param, index) => {
+                return (
+                    <Link className="menuWrapper" key={index} href={param.href}>
+                        <div className="logoMenu">{param.src}</div>
+                        <div className="menuDesc">{param.label}</div>
+                    </Link>
+                )
+            })}
+        </div>   
     )
 }

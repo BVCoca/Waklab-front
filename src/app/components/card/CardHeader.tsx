032f6e11c532b1ab144b85f3363d37a500aeda04
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-10-23 00:45:05 +0200

    fix: css rule

--- src/app/components/card/CardHeader.tsx
+++ src/app/components/card/CardHeader.tsx
@@ -3,16 +3,17 @@ import Level from "../common/Level";
 import RarityView from "../common/RarityView";
 
 interface Props {
-    level : number[],
-    rarity : Rarity|null
+  level: number[];
+  rarity: Rarity | null;
+  isSmall?: boolean;
 }
 
 // Affiche le bloc supérieur de la carte avec la rareté et le level
-export default function CardHeader({level, rarity} : Props) {
-    return (
-        <div className="cardHeader">
-            {rarity && <RarityView rarity={rarity} />}
-            <Level level={level}/>
-        </div>
-    )
+export default function CardHeader({ level, rarity, isSmall = false }: Props) {
+  return (
+    <div className="cardHeader">
+      {rarity && <RarityView rarity={rarity} isSmall={isSmall} />}
+      <Level level={level} />
+    </div>
+  );
 }

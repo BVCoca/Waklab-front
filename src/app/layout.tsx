0490d69d20632365112a61afbfd7c2ed2d49d388
Alexis Migniau <migniau.alexis@gmail.com>
2023-10-22 09:38:00 +0200

    feat(seo) : Ajout du script d'envoie des données analytics
    Oui, c'est en dur mais c'est normal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./styles/menu.css";
 import Menu from "./components/menu/menu";
 
 import type { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Waklab",
@@ -13,6 +14,19 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
       <head>
+        <Script id="analytics" dangerouslySetInnerHTML={{
+          __html: `var _paq = window._paq = window._paq || [];
+          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="//matomo.xixidev.fr/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '1']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+          })();`
+        }} />
       </head>
       <body>
         <main>

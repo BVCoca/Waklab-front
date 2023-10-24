51b2b8b84c8d13426773da480f4e2ac4b53f4f13
Rafael CDS <rafael.candidodasilva.pro@gmail.com>
2023-10-24 20:59:15 +0200

    feat(workOn): searchBar component and css homepage

--- src/app/components/header/search/search.tsx
+++ src/app/components/header/search/search.tsx
@@ -4,6 +4,7 @@ import "./search.css"
 import { useEffect, useState } from "react"
 import SearchInfiniteScroll from "./searchInfiniteScroll"
 import SearchInput from "./searchInput"
+import SearchFilter from "./searchFilter"
 import { search } from "@/app/services/common"
 import Mob from "@/app/types/Mob/Mob"
 import Stuff from "@/app/types/Stuff/Stuff"
@@ -15,39 +16,8 @@ export default function Search() {
     const [value, setValue] = useState('')
     const [page, setPage] = useState(1);
     const [loading, setLoading] = useState(false)
-
-    function handleChange(newValue: string) {
-        setValue(newValue)
-    }
-
-    useEffect(() => {
-        let timeout: NodeJS.Timeout | null = null;
-
-        if (timeout) {
-            clearTimeout(timeout)
-        }
-
-        timeout = setTimeout(() => {
-            setResults([]) 
-            setPage(1);
-            if (value.length >= 3) {
-                LoadEntity();
-            }
-        }, 500);
-        
-        return () => {
-            if (timeout) {
-                clearTimeout(timeout);
-            }
-        }
-    }, [value, page])
-
-    async function LoadEntity() {
-        setLoading(true)
-        const searchResults = await search(value, page)
-        setResults(prevResults => [...prevResults, ...searchResults["hydra:member"]])
-        setLoading(false)
-    }
+    const [isFinished, setIsFinished] = useState<string>()
+    const [totalItems, setTotalItems] = useState<number>()
 
     useEffect(() => {
         function handleScroll() {
@@ -62,15 +32,60 @@ export default function Search() {
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
+    useEffect(() => {
+        let timeout: NodeJS.Timeout | null = null;
+
+        if (timeout) {
+            clearTimeout(timeout)
+        }
+
+        timeout = setTimeout(() => {
+            setResults([])
+            setTotalItems(undefined)
+            if (value.length > 3) {
+                setPage(1);
+                LoadEntity();
+            }
+        }, 500);
+        
+        return () => {
+            if (timeout) {
+                clearTimeout(timeout);
+            }
+        }
+    }, [value])
+
+    useEffect(() => {
+        if (value.length > 3) {
+            LoadEntity();
+        }
+    }, [page]);
+
+    function handleChange(newValue: string) {
+        setValue(newValue)
+    }
+
+    async function LoadEntity() {
+        if (results.length === totalItems) {
+            setIsFinished('Aucun résultats de plus pour cette recherche')
+            setLoading(false)
+        } else {
+            setLoading(true)
+        }
+        const searchResults = await search(value, page)
+        setTotalItems(searchResults['hydra:totalItems'])
+        setResults(prevResults => [...prevResults, ...searchResults["hydra:member"]])
+        setLoading(false)
+    }
+
     return (
         <div id="searchContainer">
+            {totalItems && <div id="totalItems">{totalItems} résultats</div>}
             <SearchInput valueInput={value} onChange={handleChange}/>
-            {loading  === false
-            ? 
+            <SearchFilter/>
             <SearchInfiniteScroll resultsScroll={results}/>
-            :
-            <span className="loader"></span>
-            }
+            {loading && <div id="loaderWrapper"><span className="loader"></span></div>}  
+            {isFinished && <div id="ending"><div id="endingMessage">{isFinished}</div><a href="#top" id="backToTheTop">Retour en Haut</a></div> }
         </div>
     )
 }

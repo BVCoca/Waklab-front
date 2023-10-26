a06cf2b910bfe64d382690d182a78d30d8fc4b22
Alexis Migniau <migniau.alexis@gmail.com>
2023-10-26 20:34:24 +0200

    feat(resource) : Ajout fonctionnalité de tri

--- src/app/components/recipe/UsedInRecipe.tsx
+++ src/app/components/recipe/UsedInRecipe.tsx
@@ -7,6 +7,9 @@ import { useEffect, useState } from "react";
 import "./UsedInRecipe.css"
 import Image from "next/image";
 import LongCard from "../card/LongCard";
+import Recipe from "@/app/types/Recipe/Recipe";
+import Resource from "@/app/types/Resource/Resource";
+import Stuff from "@/app/types/Stuff/Stuff";
 
 interface Props {
     recipeIngredients : RecipeIngredient[]
@@ -44,7 +47,7 @@ export default function UsedInRecipe({recipeIngredients} : Props) {
         }, {})
 
         setJobs(tmp_job)
-        setIngredients(grouped)
+        setIngredients( sortOptions[selectedSort].callBack(grouped))
         setActiveTab(tmp_job[0]["@id"])
     }, [])
 
@@ -53,9 +56,87 @@ export default function UsedInRecipe({recipeIngredients} : Props) {
         setCurrentIngredients(ingredients[activeTab])
     }, [activeTab])
 
+    // Truc de tri
+    const sort = (ingredientsToSort :any, compare : (a : Resource|Stuff|undefined, b : Resource|Stuff|undefined) => number) : any => { 
+        Object.keys(ingredientsToSort).forEach((key : string) => {
+            ingredientsToSort[key].sort((a : RecipeFromRecipeIngredient, b : RecipeFromRecipeIngredient) => {
+
+                const entityA = a.resource ?? a.stuff
+                const entityB = b.resource ?? b.stuff
+
+                return compare(entityA, entityB)
+            })
+        })
+
+        return ingredientsToSort
+    }
+
+    const sortByLevelAsc = (ingredientsToSort : any) => {
+        return sort(ingredientsToSort, (a : Resource|Stuff|undefined,  b: Resource|Stuff|undefined) : number => {
+            return (a?.level ?? 0) - (b?.level ?? 0)
+        })
+    }
+
+    const sortByLevelDesc = (ingredientsToSort : any) => {
+        return sort(ingredientsToSort, (a : Resource|Stuff|undefined,  b: Resource|Stuff|undefined) : number => {
+            return (b?.level ?? 0) - (a?.level ?? 0)
+        })
+    }
+
+    const sortByRarityAsc = (ingredientsToSort : any) => {
+        return sort(ingredientsToSort, (a : Resource|Stuff|undefined,  b: Resource|Stuff|undefined) : number => {
+            return (a?.rarity.value ?? 0) - (b?.rarity.value ?? 0)
+        })
+    }
+
+    const sortByRarityDesc = (ingredientsToSort : any) => {
+        return sort(ingredientsToSort, (a : Resource|Stuff|undefined,  b: Resource|Stuff|undefined) : number => {
+            return (b?.rarity.value ?? 0) - (a?.rarity.value ?? 0)
+        })
+    }
+
+    // Liste les différents type de tri
+    const sortOptions = [
+        {
+            'label' : "Trier par niveau croissant",
+            'callBack' : sortByLevelAsc
+        },
+        {
+            'label' : "Trier par niveau décroissant",
+            'callBack' : sortByLevelDesc
+        },
+        {
+            'label' : "Trier du moins rare au plus rare",
+            'callBack' : sortByRarityAsc
+        },
+        {
+            'label' : "Trier du plus rare au moins rare",
+            'callBack' : sortByRarityDesc
+        },
+    ]
+
+    const handleSortSelected = (e : any) => {
+        setSelectedSort(e.target.value)
+    }
+
+    const [selectedSort, setSelectedSort] = useState<number>(0);
+
+    useEffect(() => {
+        if(Object.keys(ingredients).length > 0)
+        {
+            sortOptions[selectedSort].callBack(ingredients)
+            setCurrentIngredients([...ingredients[activeTab]])
+        }
+    }, [selectedSort])
+
     return activeTab !== '' && (
         <div className="recipeIngredientContainer">
             <h2 className="titleRecipeIngredient">Utilisée dans les recettes</h2>
+            <select className="sortSelect" onChange={handleSortSelected}>
+                {sortOptions && sortOptions.map((sort, key) => (
+                    <option key={`sort-${key}`} value={key} selected={key === selectedSort}>{sort.label}</option>
+                ))}
+            </select>
             <div className="tabContainer">
                 <ul className="headTabContainer">
                     {jobs && jobs.map(job => (

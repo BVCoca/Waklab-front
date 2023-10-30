ba24cd188c6274464d627cee9f00273af0e061ec
Vaique <vaique.benjamin@gmail.com>
2023-10-30 23:13:29 +0100

    fix(style): dungeon page and mention

--- src/app/components/dungeon/dungeonDetails.tsx
+++ src/app/components/dungeon/dungeonDetails.tsx
@@ -1,41 +1,60 @@
-import "../../dungeons/[slug]/dungeonDetails.css"
+import "../../dungeons/[slug]/dungeonDetails.css";
 import Image from "next/image";
 import Level from "@/app/components/common/Level";
 import DungeonSingle from "@/app/types/Dungeon/DungeonSingle";
 import React from "react";
 
 interface Props {
-    dungeon: DungeonSingle,
+  dungeon: DungeonSingle;
 }
 
-export default function DungeonDetails({dungeon} : Props) {
+export default function DungeonDetails({ dungeon }: Props) {
+  let IconPlayer = "/dungeonsIcon/joueur.png";
+  let IconRoom: string;
 
-    let IconPlayer = "/dungeonsIcon/joueur.png";
-    let IconRoom: string;
+  if (dungeon.room_count === 2) {
+    IconRoom = "/dungeonsIcon/tour2s.png";
+  } else if (dungeon.room_count === 3) {
+    IconRoom = "/dungeonsIcon/tour3s.png";
+  } else {
+    IconRoom = "/dungeonsIcon/tour4s.png";
+  }
 
-    if (dungeon.room_count === 2) {
-        IconRoom = "/dungeonsIcon/tour2s.png"
-    } else if (dungeon.room_count === 3) {
-        IconRoom = "/dungeonsIcon/tour3s.png"
-    } else {
-        IconRoom = "/dungeonsIcon/tour4s.png"
-    }
-
-    return (
+  return (
     <div className="detailsContent">
-        <div className="headerDetails">
-            <div className="imageContainer">
-                <Image src={dungeon.imageUrl} width={150} height={150} alt=""/>
-            </div>
-            <div className="blockCenter">
-                <h1>{dungeon.name}</h1>
-                <div className="dungeonMaxPlayer"><p className="dungeonMaxPlayerText">Nombre de joueurs max: </p><Image className="dungeonMaxPlayerImage" src={IconPlayer} width={22} height={35} alt=""/><p>x</p><p className="dungeonMaxPlayerNumber">{dungeon.max_player}</p></div> 
-                <div className="dungeonRoomCount"><p className="dungeonRoomCountText">Nombre de salles:  </p><p className="dungeonRoomCountNumber">{dungeon.room_count}</p><Image className="dungeonRoomCountImage" src={IconRoom} width={20} height={30} alt=""/></div>
-            </div>
-            <div className="levelContainer">
-                <Level level={[dungeon.level]} />
-            </div>
+      <div className="headerDetails">
+        <div className="imageContainer">
+          <Image src={dungeon.imageUrl} width={150} height={150} alt="" />
         </div>
+        <div className="blockCenter">
+          <h1>{dungeon.name}</h1>
+          <div className="dungeonMaxPlayer">
+            <p className="dungeonMaxPlayerText">Nombre de joueurs max: </p>
+            <p className="dungeonMaxPlayerNumber">{dungeon.max_player}</p>
+            <Image
+              className="dungeonMaxPlayerImage"
+              src={IconPlayer}
+              width={35}
+              height={35}
+              alt=""
+            />
+          </div>
+          <div className="dungeonRoomCount">
+            <p className="dungeonRoomCountText">Nombre de salles: </p>
+            <p className="dungeonRoomCountNumber">{dungeon.room_count}</p>
+            <Image
+              className="dungeonRoomCountImage"
+              src={IconRoom}
+              width={20}
+              height={30}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="levelContainer">
+          <Level level={[dungeon.level]} />
+        </div>
+      </div>
     </div>
-    )
+  );
 }

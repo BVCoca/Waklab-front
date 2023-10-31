70884d23a4d6694df1e9955cc709960c9bb5339b
Alexis Migniau <migniau.alexis@gmail.com>
2023-11-01 00:10:29 +0100

    fix(typo) : Correction mention légales

--- src/app/notices/page.tsx
+++ src/app/notices/page.tsx
@@ -7,7 +7,7 @@ export default function Notices() {
         <>
             <Header small={false}/>
             <NoticesText/>
-            <div id="tagForWakfu"><a href="https://www.wakfu.com/fr/mmorpg" target="_blank" id="linkWakfu">Wakfu</a><p id="textWakfu">MMORPG: © 2023 Ankama Studio. Tous droits réservés. &quot;WakLab&quot; est un site non-officiel en aucun lien avec Ankama.</p></div>
+            <div id="tagForWakfu"><a href="https://www.wakfu.com/fr/mmorpg" target="_blank" id="linkWakfu">Wakfu</a><p id="textWakfu">MMORPG: © 2023 Ankama Studio. Tous droits réservés. &quot;WakLaboratory&quot; est un site non-officiel en aucun lien avec Ankama.</p></div>
         </>
     )
   }

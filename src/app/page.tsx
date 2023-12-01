51fe62bbc0052a3ad39cc7a70cdc7fba675a49e8
Benjamin Vaique <vaique.benjamin@gmail.com>
2023-12-01 23:42:48 +0100

    fix(writting): correction desc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import "./styles/globals.css"
-import Header from "./components/header/header"
-import Count from "./components/header/count"
+import "./styles/globals.css";
+import Header from "./components/header/header";
+import Count from "./components/header/count";
 import LogoMobs from "@/app/icons/homepageIcon/logoMobs.svg";
 import LogoStuffs from "@/app/icons/homepageIcon/logoStuffs.svg";
 import LogoResources from "@/app/icons/homepageIcon/logoResources.svg";
@@ -8,59 +8,71 @@ import LogoDungeons from "@/app/icons/homepageIcon/logoDungeon.svg";
 import LogoZones from "@/app/icons/homepageIcon/logoBoussole.svg";
 
 export default function Page() {
-
   const stats = [
     {
-      'icon' : <LogoMobs width={60} height={60} />,
-      'label' : 'Monstres',
-      'number' : 796,
-      'href' : '/mobs'
+      icon: <LogoMobs width={60} height={60} />,
+      label: "Monstres",
+      number: 796,
+      href: "/mobs",
     },
     {
-      'icon' : <LogoDungeons width={40} height={60} />,
-      'label' : 'Donjons',
-      'number' : 104,
-      'href' : '/dungeons'
+      icon: <LogoDungeons width={40} height={60} />,
+      label: "Donjons",
+      number: 104,
+      href: "/dungeons",
     },
     {
-      'icon' : <LogoStuffs width={60} height={60} />,
-      'label' : 'Equipements',
-      'number' : 7114,
-      'href' : '/stuffs'
+      icon: <LogoStuffs width={60} height={60} />,
+      label: "Equipements",
+      number: 7114,
+      href: "/stuffs",
     },
     {
-      'icon' : <LogoResources width={60} height={60} />,
-      'label' : 'Ressources',
-      'number' : 1895,
-      'href' : '/resources'
+      icon: <LogoResources width={60} height={60} />,
+      label: "Ressources",
+      number: 1895,
+      href: "/resources",
     },
     {
-      'icon' : <LogoZones width={60} height={60} />,
-      'label' : 'Zones',
-      'number' : 73,
-      'href' : '/subzones'
-    }
-  ]
+      icon: <LogoZones width={60} height={60} />,
+      label: "Zones",
+      number: 73,
+      href: "/subzones",
+    },
+  ];
 
   return (
     <div className="homepageContainer">
-      <Header small={false}/>
+      <Header small={false} />
       <div className="statsContainer">
         {stats.map((stat, index) => (
-          <Count key={`stat-${index}`} label={stat.label} number={stat.number} icon={stat.icon} duration={1} href={stat.href}/>
+          <Count
+            key={`stat-${index}`}
+            label={stat.label}
+            number={stat.number}
+            icon={stat.icon}
+            duration={1}
+            href={stat.href}
+          />
         ))}
       </div>
       <div className="descriptionHome">
         <h2>Bienvenue sur Waklab,</h2>
-        <p>Le site est encore en cours de développement, nous nous excusons pour la gêne occasionnée. Vous pouvez commencer une recherche en cliquant sur une des icônes ci-dessus. Si vous voyez des erreurs ou des choses à améliorées, il y a un formulaire disponible en haut à droite de votre page. Voila ce que nous avons prévu d&apos;ajouter : </p>
+        <p>
+          Le site est encore en cours de développement, nous nous excusons pour
+          la gêne occasionnée. Vous pouvez commencer une recherche en cliquant
+          sur une des icônes ci-dessus. Si vous voyez des erreurs ou des choses
+          à améliorer, il y a un formulaire disponible en haut à droite de votre
+          page. Voila ce que nous avons prévu d&apos;ajouter :
+        </p>
         <ul>
           <li>Zones et sous zones du jeu</li>
           <li>Pages de métiers</li>
           <li>Fonctionnalité de stasis sur les donjons</li>
           <li>Ergonomie mobile</li>
         </ul>
-        <h3>Merci pour votre visite et bon jeu =)</h3> 
+        <h3>Merci pour votre visite et bon jeu =)</h3>
       </div>
     </div>
-  )
+  );
 }

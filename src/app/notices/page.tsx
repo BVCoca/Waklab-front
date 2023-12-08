31d8e68c5dec87b51369ba37ded9e9c0232f8687
Rafael CDS <rafael.candidodasilva.pro@gmail.com>
2023-12-08 17:47:40 +0100

    fix(twitter link): add target blank

--- src/app/notices/page.tsx
+++ src/app/notices/page.tsx
@@ -9,7 +9,7 @@ export default function Notices() {
             <Header small={false}/>
             <NoticesText/>
             <div className="box">
-                <a href="https://twitter.com/WakLaboratory"><Twitter className="twitterLink" style={{width: "50px"}}/></a>
+                <a href="https://twitter.com/WakLaboratory" target="_blank"><Twitter className="twitterLink" style={{width: "50px"}}/></a>
             </div>
             <div id="tagForWakfu"><a href="https://www.wakfu.com/fr/mmorpg" target="_blank" id="linkWakfu">Wakfu</a><p id="textWakfu">MMORPG: © 2023 Ankama Studio. Tous droits réservés. &quot;WakLaboratory&quot; est un site non-officiel en aucun lien avec Ankama.</p></div>
         </>
